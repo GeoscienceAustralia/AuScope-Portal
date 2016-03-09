8c09ab21b77b9bfdb735552778047696a9594e85
heyagosper <heya.gosper@ga.gov.au>
2016-03-10 10:34:24 +1100

    GPT-97--all-boreholes-drilling-date
    added drill end date to the filter query

--- src/main/java/org/auscope/portal/gsml/SF0BoreholeFilter.java
+++ src/main/java/org/auscope/portal/gsml/SF0BoreholeFilter.java
@@ -15,70 +15,61 @@ import org.springframework.stereotype.Service;
 @Service
 public class SF0BoreholeFilter extends BoreholeFilter {
 
-    // ----------------------------------------------------------- Constructors
+	// ----------------------------------------------------------- Constructors
 
-    public SF0BoreholeFilter() {
-        // test
-        super(null, null, null, null);
-    }
+	public SF0BoreholeFilter() {
+		// test
+		super(null, null, null, null);
+	}
 
-    public SF0BoreholeFilter(String boreholeName, String custodian, String dateOfDrilling, List<String> ids) {
-        super(boreholeName, custodian, dateOfDrilling, ids);
-    }
+	public SF0BoreholeFilter(String boreholeName, String custodian, String dateOfDrilling, List<String> ids) {
+		super(boreholeName, custodian, dateOfDrilling, ids);
+	}
 
-    // --------------------------------------------------------- Public Methods
+	// --------------------------------------------------------- Public Methods
 
-    @Override
-    public String getFilterStringAllRecords() {
-        return this.generateFilter(this.generateFilterFragment());
-    }
+	@Override
+	public String getFilterStringAllRecords() {
+		return this.generateFilter(this.generateFilterFragment());
+	}
 
-    @Override
-    public String getFilterStringBoundingBox(FilterBoundingBox bbox) {
+	@Override
+	public String getFilterStringBoundingBox(FilterBoundingBox bbox) {
 
-        return this
-                .generateFilter(this.generateAndComparisonFragment(
-                        this.generateBboxFragment(bbox,
-                                "gsmlp:shape"),
-                        this.generateFilterFragment()));
-    }
+		return this.generateFilter(this.generateAndComparisonFragment(this.generateBboxFragment(bbox, "gsmlp:shape"),
+				this.generateFilterFragment()));
+	}
 
-    @Override
-    protected String generateFilterFragment() {
-        List<String> parameterFragments = new ArrayList<String>();
-        if (boreholeName != null && !boreholeName.isEmpty()) {
-            parameterFragments.add(this.generatePropertyIsLikeFragment(
-                    "gsmlp:name", "*" + this.boreholeName + "*"));
-        }
+	@Override
+	protected String generateFilterFragment() {
+		List<String> parameterFragments = new ArrayList<String>();
+		if (boreholeName != null && !boreholeName.isEmpty()) {
+			parameterFragments.add(this.generatePropertyIsLikeFragment("gsmlp:name", "*" + this.boreholeName + "*"));
+		}
 
-        if (custodian != null && !custodian.isEmpty()) {
-            parameterFragments
-                    .add(this
-                            .generatePropertyIsLikeFragment(
-                                    "gsmlp:boreholeMaterialCustodian",
-                                    this.custodian));
-        }
+		if (custodian != null && !custodian.isEmpty()) {
+			parameterFragments
+					.add(this.generatePropertyIsLikeFragment("gsmlp:boreholeMaterialCustodian", this.custodian));
+		}
 
-        if (dateOfDrilling != null && !dateOfDrilling.isEmpty()) {
-            parameterFragments.add(this.generatePropertyIsLikeFragment(
-                    "gsmlp:drillStartDate",
-                    "*" +  this.dateOfDrilling + "*"));
-        }
+		if (dateOfDrilling != null && !dateOfDrilling.isEmpty()) {
+			parameterFragments.add(this.generateOrComparisonFragment(
+					this.generatePropertyIsLikeFragment("gsmlp:drillStartDate", "*" + this.dateOfDrilling + "*"),
+					this.generatePropertyIsLikeFragment("gsmlp:drillEndDate", "*" + this.dateOfDrilling + "*")));
+		}
 
-        if (this.restrictToIDList != null && !this.restrictToIDList.isEmpty()) {
-            List<String> idFragments = new ArrayList<String>();
-            for (String id : restrictToIDList) {
-                if (id != null && id.length() > 0) {
-                    idFragments.add(this.generateFeatureIdFragment("gsml.borehole." + id));
-                }
-            }
-            parameterFragments.add(this
-                    .generateOrComparisonFragment(idFragments
-                            .toArray(new String[idFragments.size()])));
-        }
+		if (this.restrictToIDList != null && !this.restrictToIDList.isEmpty()) {
+			List<String> idFragments = new ArrayList<String>();
+			for (String id : restrictToIDList) {
+				if (id != null && id.length() > 0) {
+					idFragments.add(this.generateFeatureIdFragment("gsml.borehole." + id));
+				}
+			}
+			parameterFragments
+					.add(this.generateOrComparisonFragment(idFragments.toArray(new String[idFragments.size()])));
+		}
 
-        return this.generateAndComparisonFragment(this
-                .generateAndComparisonFragment(parameterFragments
-                        .toArray(new String[parameterFragments.size()])));
-    }
+		return this.generateAndComparisonFragment(
+				this.generateAndComparisonFragment(parameterFragments.toArray(new String[parameterFragments.size()])));
+	}
 }
