af2da4d060f7fd70573d3dab6703f3f9dac6f7d7
Callum McKenna <callum.mckenna@ga.gov.au>
2022-08-10 09:09:12 +1000

    GEOPOR-853: Add environment specific CSW selector for CAPAD 2020 - Marine

--- src/main/java/au/gov/geoscience/portal/config/knownlayers/RestrictedAreas.java
+++ src/main/java/au/gov/geoscience/portal/config/knownlayers/RestrictedAreas.java
@@ -4,21 +4,28 @@ import org.auscope.portal.core.view.knownlayer.CSWRecordSelector;
 import org.auscope.portal.core.view.knownlayer.KnownLayer;
 import org.auscope.portal.core.view.knownlayer.KnownLayerSelector;
 import org.auscope.portal.core.view.knownlayer.WMSSelector;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 
 @Configuration
 public class RestrictedAreas {
-
     private static final String GROUP = "Restricted Areas";
     private static final int GROUP_ORDER = GroupOrder.RESTRICTED_AREAS;
+
+    @Value("${portal.build.stack.environment}")
+    private String stackEnvironment;
 
     @Bean(name = "protected-areas-marine")
     @Order(GROUP_ORDER)
     KnownLayer protectedAreasMarine() {
+        String recordId = stackEnvironment.equalsIgnoreCase("PROD") ? "" : "83299a1edca1ad489aebbf4998c31720526febe8";
+
         String id = "protected-areas-marine";
-        KnownLayerSelector selector = new WMSSelector("Australia_Commonwealth_Marine_Reserves");
+        CSWRecordSelector selector = new CSWRecordSelector();
+        selector.setRecordId(recordId);
+
         String name = "Protected Areas CAPAD 2020 - Marine";
 
         KnownLayer knownLayer = new KnownLayer(id, selector);
